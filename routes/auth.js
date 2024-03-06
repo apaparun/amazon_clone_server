ab4ccf8ffa981f36c57daf2075a7b9fe3b9c4617
apaparun <arunprasanth488@gmail.com>
2024-03-06 23:52:01 +0530

    signup api created

--- routes/auth.js
+++ routes/auth.js
@@ -1,29 +1,33 @@
 const express = require('express');
 const User = require('../models/user');
 
-
+const bcryptjs = require('bcryptjs')
 const authRouter = express.Router();
 
 
 authRouter.post("/api/signup", async (req, res) => {
-    //get data from client
-    const { name, email, password } = req.body;
+    try{
+        //get data from client
+        const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email })
     if(existingUser){
         return res.status(400).json({message:"User already exist"})
     }
-
+  const hashedpassword =   await bcryptjs.hash(password,8);
     let user = new User({
         email,
-        password,
+        password:hashedpassword,
         name
     })
-    user =await user.save()
-    res.json(user)
     //post data in db
-
-
+    user =await user.save()
     //return data to client
+    res.json(user)
+    }catch(e){
+        res.status(500).json({error:e.message})
+    }
+
+
 })
 
 module.exports = authRouter;
