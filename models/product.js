b35d19106d134d18d3c08133a45aea4555b018a1
apaparun <arunprasanth488@gmail.com>
2024-06-16 01:26:25 +0530

    rating api completed

--- models/product.js
+++ models/product.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+const ratingSchema = require("./rating");
+
 const productSchema = mongoose.Schema({
   name: {
     type: String,
@@ -29,6 +31,7 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  ratings: [ratingSchema],
 });
 
 const Product = mongoose.model("Product", productSchema);
